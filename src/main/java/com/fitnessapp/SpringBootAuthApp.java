f8066d87cffd2e8ec6f3e7519c48520403b5ed70
EC2 Default User <ec2-user@ip-172-31-30-255.us-east-2.compute.internal>
2021-07-10 22:26:18 +0000

    initial commit

---
+++ src/main/java/com/fitnessapp/SpringBootAuthApp.java
@@ -0,0 +1,14 @@
+package com.netjstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// https://www.netjstech.com/2021/02/spring-boot-spring-security-jwt-authentication.html
+
+@SpringBootApplication
+public class SpringBootAuthApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthApp.class, args);
+	}
+}
